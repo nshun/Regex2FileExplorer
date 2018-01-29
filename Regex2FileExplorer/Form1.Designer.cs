a2fe13b35e690acdea37c5188cb83a98a4604815
Nishimura <n.shun1110@gmail.com>
2018-01-29 20:48:26 +0900

    プロジェクト ファイルを追加します。

---
+++ Regex2FileExplorer/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Regex2FileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxRegexFrom = new System.Windows.Forms.TextBox();
+            this.textBoxRegexTo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources;
+            this.textBox1.Location = new System.Drawing.Point(50, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllSystemSources;
+            this.textBox2.Location = new System.Drawing.Point(412, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(327, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 125);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(354, 274);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(385, 125);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(354, 274);
+            this.listBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "To";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxRegexTo);
+            this.groupBox1.Controls.Add(this.textBoxRegexFrom);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(727, 78);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Regex";
+            // 
+            // textBoxRegexFrom
+            // 
+            this.textBoxRegexFrom.Location = new System.Drawing.Point(19, 22);
+            this.textBoxRegexFrom.Name = "textBoxRegexFrom";
+            this.textBoxRegexFrom.Size = new System.Drawing.Size(316, 22);
+            this.textBoxRegexFrom.TabIndex = 0;
+            // 
+            // textBoxRegexTo
+            // 
+            this.textBoxRegexTo.Location = new System.Drawing.Point(390, 22);
+            this.textBoxRegexTo.Name = "textBoxRegexTo";
+            this.textBoxRegexTo.Size = new System.Drawing.Size(321, 22);
+            this.textBoxRegexTo.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(345, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "→";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(636, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Move";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 411);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxRegexTo;
+        private System.Windows.Forms.TextBox textBoxRegexFrom;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Regex2FileExplorer/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Regex2FileExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBoxRegexFrom.KeyDown += TextBoxRegexFrom_KeyDown;
+            textBox1.KeyDown += TextBox1_KeyDown;
+            textBox2.KeyDown += TextBox2_KeyDown;
+            listBox1.MouseDoubleClick += ListBox1_MouseDoubleClick;
+            listBox2.MouseDoubleClick += ListBox2_MouseDoubleClick;
+
+        }
+        
+        private void TextBoxRegexFrom_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+
+            }
+        }
+
+        private void ListBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string path = "";
+            var fileName = listBox1.SelectedItem.ToString();
+            var dirPath = textBox1.Text;
+            if (fileName == "..") path = Directory.GetParent(dirPath).ToString();
+            else path = Path.Combine(dirPath, fileName);
+            if (Directory.Exists(path))
+            {
+                textBox1.Text = path;
+                updateList(textBox1.Text, listBox1);
+            }
+            else if(File.Exists(path))
+            {
+                System.Diagnostics.Process.Start(path);
+            }
+            
+        }
+        private void ListBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string path = "";
+            var fileName = listBox2.SelectedItem.ToString();
+            var dirPath = textBox2.Text;
+            if (fileName == "..") path = Directory.GetParent(dirPath).ToString();
+            else path = Path.Combine(dirPath, fileName);
+            if (Directory.Exists(path))
+            {
+                textBox2.Text = path;
+                updateList(textBox2.Text, listBox2);
+            }
+            else if (File.Exists(path))
+            {
+                System.Diagnostics.Process.Start(path);
+            }
+        }
+
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && Directory.Exists(textBox1.Text))
+            {
+                updateList(textBox1.Text, listBox1);
+                if (string.IsNullOrEmpty(textBox2.Text))
+                {
+                    textBox2.Text = textBox1.Text;
+                    updateList(textBox1.Text, listBox2);
+                }
+            }
+        }
+
+        private void TextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && Directory.Exists(textBox1.Text))
+            {
+                updateList(textBox2.Text, listBox2);
+                if (string.IsNullOrEmpty(textBox1.Text))
+                {
+                    textBox2.Text = textBox1.Text;
+                    updateList(textBox2.Text, listBox1);
+                }
+            }
+        }
+
+        private void updateList(string path, ListBox listBox)
+        {
+            string[] filesName = getFileDirNames(path);
+            listBox.Items.Clear();
+            try
+            {
+                var parent = Directory.GetParent(path).ToString();
+                listBox.Items.Add("..");
+            }
+            catch
+            {
+
+            }
+            listBox.Items.AddRange(filesName);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fromFiles = getFileNames(textBox1.Text);
+            var fromPattern = textBoxRegexFrom.Text;
+            var toPattern = textBoxRegexTo.Text;
+            foreach (var file in fromFiles)
+            {
+                if(Regex.IsMatch(file, fromPattern))
+                {
+                    var toFile = Regex.Replace(file, fromPattern, toPattern);
+                    var sourcePath = new Uri(new Uri(textBox1.Text+@"\"), file).LocalPath;
+                    var destPath = new Uri(new Uri(textBox2.Text + @"\"), toFile).LocalPath;
+                    var destDir = Path.GetDirectoryName(destPath);
+                    if (!Directory.Exists(destDir))
+                        Directory.CreateDirectory(destDir);
+                    File.Move(sourcePath, destPath);
+                }
+            }
+            updateList(textBox1.Text, listBox1);
+            updateList(textBox2.Text, listBox2);
+
+        }
+
+        private string[] getFileDirNames(string path)
+        {
+            string[] files = Directory.GetFileSystemEntries(path, "*", System.IO.SearchOption.TopDirectoryOnly);
+            string[] filesName = new string[files.Count()];
+            for (int i = 0; i < files.Count(); i++)
+            {
+                var name = Path.GetFileName(files[i]);
+                filesName[i] = name;
+            }
+            return filesName;
+        }
+
+        private string[] getFileNames(string path)
+        {
+            string[] files = Directory.GetFiles(path, "*", System.IO.SearchOption.TopDirectoryOnly);
+            string[] filesName = new string[files.Count()];
+            for (int i = 0; i < files.Count(); i++)
+            {
+                var name = Path.GetFileName(files[i]);
+                filesName[i] = name;
+            }
+            return filesName;
+        }
+    }
+}
